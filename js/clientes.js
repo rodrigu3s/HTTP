31b4979a4ed5686bb1c609295b33f739d7268e8d
joao <joaorodrigues9631@gmail.com>
2021-05-11 19:19:33 -0300

    separando arquivos

--- js/clientes.js
+++ js/clientes.js
@@ -1,47 +1,40 @@
 
-//Crio templete
-const criaNovaLinha = (name, email, phone, id) =>{
-    const linhaNovoCliente = document.createElement('tr')
-    const conteudo = `
+const createNewRow = (name, email, phone, id) =>{
+    const clientNewRow = document.createElement('tr')
+    const content = `
                 <td class="td" data-td>${name}</td>
                 <td  class="td">${email}</td>
                 <td  class="td">${phone}</td>
-                <td><button onclick="editar(${id})">Editar</button></td>
-                <td><button onclick="deletar(${id})">Deletar</button></td>
+                <td><button onclick="edit(${id})">Editar</button></td>
+                <td><button onclick="deleteClient(${id})">Deletar</button></td>
                
                 `
-    linhaNovoCliente.innerHTML = conteudo   //atribuo as td a tr 
-    return linhaNovoCliente
+    clientNewRow.innerHTML = content   
+    return clientNewRow
 }
+ 
+const tabela = document.querySelector('[data-table]')
 
-// reservo um lugar p/ o templete 
-const tabela = document.querySelector('[data-tabela]')
-
-const listaClientes = ()=>{
-    return fetch(`http://localhost:3000/clientes`) // a fetch me retorna uma promisse
-    .then( resposta =>{
-        return resposta.json()    //a resposta por padrão é texto, então tranformo em objeto js
+const listClient = ()=>{
+    return fetch(`http://localhost:3000/clientes`) 
+    .then( response =>{
+        return response.json()    
     })
 }
-
-// Passa os dados para o templete
-listaClientes()
+listClient()
 .then(data =>{
     data.forEach(elemento =>{
-    //Atribuo o conteudo da tabela criada dentro da mminha tabela no lista-cliente.html
-    tabela.appendChild(criaNovaLinha(elemento.name, elemento.email, elemento.phone, elemento.id))    
+        tabela.appendChild(createNewRow(elemento.name, elemento.email, elemento.phone, elemento.id))    
     })
 })
 
-
-const criaClient = ( name, email, phone, id) =>{
+const createClient = ( name, email, phone, id) =>{
     const myid = id 
-    console.log(myid);
     if(myid){
         return fetch(`http://localhost:3000/clientes/${myid}`,{
             method: 'PUT',
             headers:{
-                // 'Accept': 'application/json',
+                'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
@@ -53,11 +46,10 @@ const criaClient = ( name, email, phone, id) =>{
 
         })
     }else{
-        // console.log(name, email, phone)
         return fetch(`http://localhost:3000/clientes`,{
             method: 'POST',
             headers:{
-                // 'Accept': 'application/json',
+                'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
@@ -67,62 +59,47 @@ const criaClient = ( name, email, phone, id) =>{
             })
         })
 
-        .then( resposta=>{
-            return resposta.body
+        .then( response=>{
+            return response.body
         })
     }
-
-   
 }
 
-const formulario = document.querySelector('[data-form]')
-formulario.addEventListener('submit', (evento)=>{
-    evento.preventDefault()
-    const name = evento.target.querySelector('[data-name]').value
-    const email = evento.target.querySelector('[data-email]').value
-    const phone = evento.target.querySelector('[data-phone]').value
-    const id = evento.target.querySelector('[data-id]').value
+const form = document.querySelector('[data-form]')
+form.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    const name = event.target.querySelector('[data-name]').value
+    const email = event.target.querySelector('[data-email]').value
+    const phone = event.target.querySelector('[data-phone]').value
+    const id = event.target.querySelector('[data-id]').value
 
-    console.log(name)
-    criaClient(name, email, phone, id )
+    createClient(name, email, phone, id )
 })
 
-
-// Deletar 
-function deletar(id){
+ 
+function deleteClient(id){
     return fetch(`http://localhost:3000/clientes/${id}`,{
         method: 'DELETE' 
     })
 }
 
 
-function editar(id){
+function edit(id){
     fetch(`http://localhost:3000/clientes/${id}`,{
         method: "GET" 
     })
 
-    .then( resposta=>{
-        return resposta.json()
+    .then( response=>{
+        return response.json()
     })
     .then(data =>{
-        console.log(data);
         document.getElementById("id").value = data.id
         document.getElementById("name").value = data.name
         document.getElementById("email").value = data.email
         document.getElementById("phone").value = data.phone
     })
-
-    
-    
-    
-    
-    
-    // consulta id
-    // retorna obj dentro do form
-    // condição adicionar or edit
-     
-    
 }
+
 
 
 
